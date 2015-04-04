0eecd3c1cfa7d4b2901b912e7ce3e408f1f096c1
Shengkun Wei <shengkunwei@gmail.com>
2015-04-04 00:58:16 -0400

    .

--- src/HuffmanProject5565.java
+++ src/HuffmanProject5565.java
@@ -1,8 +1,54 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 
 public class HuffmanProject5565 {
-
+	
+	static final int KEY_BYTE_NUM = 2;
+	
 	public static void main(String[] args) {
-		
+		HashMap<Integer, Integer> freqMap = readFile("liberty.jpg");
+		int i = 0;
+		for(Entry<Integer, Integer> entry : freqMap.entrySet()){
+			System.out.println(i ++ + " - " + entry.getKey() + "  :  " + entry.getValue());
+		}
 	}
 
+	private static HashMap<Integer, Integer> readFile(String filename){
+		HashMap<Integer, Integer> freqMap = new HashMap<>();
+		BufferedInputStream bis = null;
+		try {
+			bis = new BufferedInputStream(new FileInputStream(filename));
+			int b = 0;
+			while((b = bis.read()) != -1){
+				Integer value = freqMap.get(b);
+				if(value == null){
+					freqMap.put(b, 1);
+				} else {
+					freqMap.put(b, value + 1);
+				}
+				
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(bis != null){
+				try {
+					bis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return freqMap;
+	}
+	
 }
---
+++ src/MinHeap.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+
+public class MinHeap {
+	
+	private HashMap<Integer, Integer> freqMap;
+
+	public MinHeap(HashMap<Integer, Integer> freqMap) {
+		this.freqMap = freqMap;
+	}
+	
+	
+}
